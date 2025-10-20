ecefdd5f808ef456f0b6d980d3bd54f2001438ef
Jeff Molter <jeffmolter@Jeffs-MBP.attlocal.net>
2025-10-19 23:03:35 -0400

    site config

--- src/data/SampleData.ts
+++ src/data/SampleData.ts
@@ -1,87 +1,146 @@
 import { Technique } from "../state/types";
 
+
 export const sampleTechniques: Technique[] = [
-  {
-    id: '1',
-    name: 'Closed Guard',
-    paths: ['Position > Ground > Guard > Closed Guard'],
-    description: 'Fundamental guard position with legs locked around opponent',
-    relatedTechniques: ['2', '3', '4']
-  },
 
   {
-    id: '2',
-    name: 'Triangle Choke',
-    paths: [
-      'Submission > Ground > Guard > Triangle'
+    "id": "1",
+    "name": "Closed Guard",
+    "paths": [
+      "Position > Ground > Guard > Closed Guard"
     ],
-    description: 'Choke using legs from guard, also a controlling position',
-    relatedTechniques: ['1', '3']
+    "description": "Fundamental guard position with legs locked around opponent",
+    "relatedTechniques": [
+      "2",
+      "3",
+      "4"
+    ]
   },
   {
-    id: '3',
-    name: 'Armbar from Guard',
-    paths: ['Submission > Ground > Guard > Armbar'],
-    description: 'Classic submission attacking the elbow',
-    relatedTechniques: ['1', '2']
+    "id": "2",
+    "name": "Triangle Choke",
+    "paths": [
+      "Submission > Ground > Guard > Triangle"
+    ],
+    "description": "Choke using legs from guard, also a controlling position",
+    "relatedTechniques": [
+      "1",
+      "3"
+    ]
   },
   {
-    id: '4',
-    name: 'Scissor Sweep',
-    paths: ['Transition > Ground > Guard > Sweep'],
-    description: 'Fundamental sweep from closed guard',
-    relatedTechniques: ['1']
+    "id": "3",
+    "name": "Armbar from Guard",
+    "paths": [
+      "Submission > Ground > Guard > Armbar"
+    ],
+    "description": "Classic submission attacking the elbow",
+    "relatedTechniques": [
+      "1",
+      "2"
+    ]
   },
   {
-    id: '5',
-    name: 'Turtle',
-    paths: ['Position > Ground > Turtle'],
-    description: 'Position generally attacking from with oppenent balled up in prone position',
-    relatedTechniques: []
+    "id": "4",
+    "name": "Scissor Sweep",
+    "paths": [
+      "Transition > Ground > Guard > Sweep"
+    ],
+    "description": "Fundamental sweep from closed guard",
+    "relatedTechniques": [
+      "1"
+    ]
   },
   {
-    id: '6',
-    name: 'Side Control',
-    paths: ['Position > Ground > Side Control'],
-    description: 'Position generally attacking from with oppenent balled up in prone position',
-    relatedTechniques: []
+    "name": "Turtle",
+    "paths": [
+      "Position > Ground > Turtle"
+    ],
+    "description": "Position generally attacking from with oppenent balled up in prone position",
+    "relatedTechniques": [
+      "1760890170877"
+    ],
+    "id": "5"
   },
   {
-    id: '7',
-    name: 'Mount',
-    paths: ['Position > Ground > Mount'],
-    description: 'Position generally attacking from with oppenent balled up in prone position',
-    relatedTechniques: []
+    "id": "6",
+    "name": "Side Control",
+    "paths": [
+      "Position > Ground > Side Control"
+    ],
+    "description": "Position generally attacking from with oppenent balled up in prone position",
+    "relatedTechniques": []
   },
   {
-    id: '8',
-    name: 'Half Guard',
-    paths: ['Position > Ground > Half Guard'],
-    description: 'Position generally attacking from with oppenent balled up in prone position',
-    relatedTechniques: []
+    "id": "7",
+    "name": "Mount",
+    "paths": [
+      "Position > Ground > Mount"
+    ],
+    "description": "Position generally attacking from with oppenent balled up in prone position",
+    "relatedTechniques": []
   },
   {
-    id: '9',
-    name: 'Back Control',
-    paths: ['Position > Ground > Back Control'],
-    description: 'Position generally attacking from with oppenent balled up in prone position',
-    relatedTechniques: []
+    "id": "8",
+    "name": "Half Guard",
+    "paths": [
+      "Position > Ground > Half Guard"
+    ],
+    "description": "Position generally attacking from with oppenent balled up in prone position",
+    "relatedTechniques": []
   },
   {
-    id: '10',
-    name: 'Rear Nake Choke',
-    paths: ['Submission > Back Control'],
-    description: 'Mate Leon',
-    relatedTechniques: ['9']
+    "id": "9",
+    "name": "Back Control",
+    "paths": [
+      "Position > Ground > Back Control"
+    ],
+    "description": "Position generally attacking from with oppenent balled up in prone position",
+    "relatedTechniques": []
   },
   {
-    id: '11',
-    name: 'Standing',
-    paths: ['Position > Standing'],
-    description: 'Fundamental guard position with legs locked around opponent',
-    relatedTechniques: []
+    "name": "Rear Nake Choke",
+    "paths": [
+      "Submission > Ground > Back Control > Top"
+    ],
+    "description": "Mate Leon",
+    "relatedTechniques": [
+      "9"
+    ],
+    "id": "10"
   },
-];
+  {
+    "id": "11",
+    "name": "Standing",
+    "paths": [
+      "Position > Standing"
+    ],
+    "description": "Fundamental guard position with legs locked around opponent",
+    "relatedTechniques": []
+  },
+  {
+    "name": "Clock Choke",
+    "paths": [
+      "Submission > Ground > Turtle > Turtle Top"
+    ],
+    "description": "",
+    "relatedTechniques": [
+      "5"
+    ],
+    "id": "1760890170877"
+  },
+  {
+    "name": "John Wayne Sweep",
+    "paths": [
+      "Transition > Ground > Half Guard > Bottom"
+    ],
+    "description": "",
+    "relatedTechniques": [
+      "8"
+    ],
+    "id": "1760890227276"
+  }
+]
 
 export const sampleData: { [key: string]: any } = {
   'Position': {
